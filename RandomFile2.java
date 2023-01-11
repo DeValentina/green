f7f3bdfa9123e086c97d2c8eabcdb1f656dc75ae
Valen-Lee <120369051+Valen-Lee@users.noreply.github.com>
2023-01-11 12:31:47 +0900

    Add files via upload

---
+++ RandomFile2.java
@@ -0,0 +1,25 @@
+package ch18;
+
+import java.io.RandomAccessFile;
+
+public class RandomFile2 {
+	public static void main(String[] args) {
+		String str=null;
+		try {
+			RandomAccessFile file = new RandomAccessFile("c:\\test\\rand2.txt", "rw");
+			file.seek(9); 
+			file.write("park".getBytes());
+			while (file.getFilePointer() < file.length()) {
+				//파일의 내용보다 파일포인터의 위치값이 적으면 반복
+				str=file.readLine();
+				str = new String(str.getBytes("8859_1"),"utf-8");
+			}
+			file.close();//파일 저장시점
+			System.out.println("파일이 저장되었습니다.");
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
